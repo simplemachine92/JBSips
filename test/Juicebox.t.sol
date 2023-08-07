50bb14528330d582fee650211711897071389406
Justin Pulley <justinpulley92@gmail.com>
2023-08-07 15:07:42 -0500

    chore: init from https://github.com/simplemachine92/JB311-Template at 437f918

---
+++ test/Juicebox.t.sol
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./helpers/TestBaseWorkflowV3.sol";
+import "@jbx-protocol/juice-delegates-registry/src/JBDelegatesRegistry.sol";
+
+import {IJBDelegatesRegistry} from "@jbx-protocol/juice-delegates-registry/src/interfaces/IJBDelegatesRegistry.sol";
+import {IJBFundingCycleBallot} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBFundingCycleBallot.sol";
+import {JBGlobalFundingCycleMetadata} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBFundingCycleMetadata.sol";
+import {JBOperatorData} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBOperatorData.sol";
+
+import {Test, console2} from "forge-std/Test.sol";
+
+contract CounterTest is TestBaseWorkflowV3 {
+
+    using JBFundingCycleMetadataResolver for JBFundingCycle;
+
+    // Project setup params
+    JBProjectMetadata _projectMetadata;
+    JBFundingCycleData _data;
+    JBFundingCycleMetadata _metadata;
+    JBFundAccessConstraints[] _fundAccessConstraints; // Default empty
+    IJBPaymentTerminal[] _terminals; // Default empty
+    /* IJBStrawsDelegate _straws;
+    Merkle _m; */
+
+    // Delegate setup params
+    JBDelegatesRegistry delegatesRegistry;
+    /* JBStraws _delegateImpl;
+    JBStrawsDeployer _delegateDepl;
+    DeployJBStrawsData delegateData;
+    JBStrawsProjectDeployer projectDepl; */
+
+    /* // Assigned when project is launched
+    uint256 _projectId;
+
+    // Used in JBFundingCycleMetadata, 4500 = 45% I believe, but using 0 for testing calcs
+    uint256 reservedRate = 0;
+
+    // Used in JBFundingCycleData
+    uint256 weight = 10 ** 18; // Minting 1 token per eth
+
+    bytes32 tRoot;
+    bytes32 tRoot2; */
+
+    function setUp() public override {
+        // Provides us with _jbOperatorStore and _jbETHPaymentTerminal
+        super.setUp();
+
+        /* 
+        This setup follows a DelegateProjectDeployer pattern like in https://docs.juicebox.money/dev/extensions/juice-721-delegate/
+        It deploys a new JB project and funding cycle, and then attaches our delegate to that funding cycle as a DataSource and Delegate.
+        */
+
+        // Placeholder project metadata, would customize this in prod.
+        _projectMetadata = JBProjectMetadata({content: "myIPFSHash", domain: 1});
+
+        // https://docs.juicebox.money/dev/extensions/juice-delegates-registry/jbdelegatesregistry/
+        delegatesRegistry = new JBDelegatesRegistry(IJBDelegatesRegistry(address(0)));
+
+        /* // Instance of our delegate code
+        _delegateImpl = new JBStraws(_jbOperatorStore);
+
+        // Required for our custom project deployer below, eventually attaches the delegate to the funding cycle.
+        _delegateDepl = new JBStrawsDeployer(_delegateImpl, delegatesRegistry);
+
+        // Custom deployer
+        projectDepl = new JBStrawsProjectDeployer(_delegateDepl, _jbOperatorStore);
+ 
+        */
+
+        address _delegateImpl = address(0);
+        // The following describes the funding cycle, access constraints, and metadata necessary for our project.
+        _data = JBFundingCycleData({
+            duration: 30 days,
+            weight: 10 ** 18,
+            discountRate: 0,
+            ballot: IJBFundingCycleBallot(address(0))
+        });
+
+        _metadata = JBFundingCycleMetadata({
+            global: JBGlobalFundingCycleMetadata({
+                allowSetTerminals: false,
+                allowSetController: false,
+                pauseTransfers: false
+            }),
+            reservedRate: 0,
+            redemptionRate: 5000,
+            ballotRedemptionRate: 0,
+            pausePay: false,
+            pauseDistributions: false,
+            pauseRedeem: false,
+            pauseBurn: false,
+            allowMinting: true,
+            preferClaimedTokenOverride: false,
+            allowTerminalMigration: false,
+            allowControllerMigration: false,
+            holdFees: false,
+            useTotalOverflowForRedemptions: false,
+            useDataSourceForPay: true,
+            useDataSourceForRedeem: false,
+            dataSource: address(_delegateImpl),
+            metadata: 0
+        });
+
+        _fundAccessConstraints.push(
+            JBFundAccessConstraints({
+                terminal: _jbETHPaymentTerminal,
+                token: jbLibraries().ETHToken(),
+                distributionLimit: 2 ether,
+                overflowAllowance: type(uint232).max,
+                distributionLimitCurrency: 1, // Currency = ETH
+                overflowAllowanceCurrency: 1
+            })
+        );
+
+        // Imported from TestBaseWorkflowV3.sol via super.setUp() https://docs.juicebox.money/dev/learn/architecture/terminals/
+        _terminals = [_jbETHPaymentTerminal];
+
+        /* JBGroupedSplits[] memory _groupedSplits = new JBGroupedSplits[](1); // Default empty */
+
+        /* // The imported struct used by our delegate
+        delegateData = DeployJBStrawsData({
+            initPayRoot: tRoot,
+            initRedeemRoot: tRoot2,
+            initPayWL: false,
+            initRedeemWL: false
+        });
+
+        // Assemble all of our previous configuration for our project deployer
+         LaunchProjectData memory launchProjectData = LaunchProjectData({
+            projectMetadata: _projectMetadata,
+            data: _data,
+            metadata: _metadata,
+            mustStartAtOrAfter: 0,
+            groupedSplits: _groupedSplits,
+            fundAccessConstraints: _fundAccessConstraints,
+            terminals: _terminals,
+            memo: ""
+        });
+
+
+        // Blastoff
+        vm.prank(address(123));
+        _projectId = projectDepl.launchProjectFor(
+            address(123),
+            delegateData,
+            launchProjectData,
+            _jbController
+        ); */
+
+        (, JBFundingCycleMetadata memory metadata, ) = _jbController.latestConfiguredFundingCycleOf(1);
+
+        vm.label(metadata.dataSource, "Initialized DS");
+
+        /* _straws = IJBStrawsDelegate(metadata.dataSource); */
+    }
+
+}
