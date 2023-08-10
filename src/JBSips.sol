e89fd78a30a6d99db55a7089c6b8b9daa2a0acaa
Justin Pulley <justinpulley92@gmail.com>
2023-08-09 21:52:51 -0500

    feat: access control, begin allocate conditional logic

--- src/JBSips.sol
+++ src/JBSips.sol
@@ -2,10 +2,25 @@
 pragma solidity ^0.8.20;
 
 import {IJBSplitAllocator} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBSplitAllocator.sol";
+import {IJBPaymentTerminal} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPaymentTerminal.sol";
+import {IJBDirectory} from '@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBDirectory.sol';
+import {IJBOperatorStore} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBOperatorStore.sol";
 import {JBSplitAllocationData} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBSplitAllocationData.sol";
+import {JBOperatable} from "@jbx-protocol/juice-contracts-v3/contracts/abstract/JBOperatable.sol";
 
-contract JBSips is IJBSplitAllocator {
+contract JBSips is IJBSplitAllocator, JBOperatable {
 
+    //*********************************************************************//
+    // --------------------------- custom errors ------------------------- //
+    //*********************************************************************//
+
+    error JuiceSips_Unauthorized();
+
+    IJBDirectory public immutable DIRECTORY;
+
+    constructor(IJBDirectory _directory, IJBOperatorStore _operatorStore) JBOperatable(_operatorStore) {
+        DIRECTORY = _directory;
+    }
 
     /// @notice Indicates if this contract adheres to the specified interface.
     /// @dev See {IERC165-supportsInterface}.
@@ -15,9 +30,19 @@ contract JBSips is IJBSplitAllocator {
         return _interfaceId == type(IJBSplitAllocator).interfaceId;
     }
 
-    
     /// @notice Called by a project's payout (JBTerminal) or reserved token distribution split (JBController)
     /// @dev See https://docs.juicebox.money/dev/learn/glossary/split-allocator/
     /// @param _data See https://docs.juicebox.money/dev/api/data-structures/jbsplitallocationdata/
-    function allocate(JBSplitAllocationData calldata _data) external payable {}
+    function allocate(JBSplitAllocationData calldata _data) external payable {
+        // Ensure call is coming from Terminal or Controller
+        if (!DIRECTORY.isTerminalOf(_data.projectId, IJBPaymentTerminal(msg.sender)) && DIRECTORY.controllerOf(_data.projectId) != msg.sender)
+            revert JuiceSips_Unauthorized();
+        
+        // Logic for payouts
+        if (DIRECTORY.isTerminalOf(_data.projectId, IJBPaymentTerminal(msg.sender))) {}
+
+        /* // Logic for reserved token ditribution split (bonus implementation, not the focus rn)
+        if (DIRECTORY.controllerOf(_data.projectId) == msg.sender) {} */
+
+    }
 }
