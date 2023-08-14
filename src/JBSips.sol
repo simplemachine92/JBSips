de204af887af023fa8fd386ad6c9d5aa8a319732
Justin Pulley <justinpulley92@gmail.com>
2023-08-14 13:08:08 -0500

    checkp: abstract contract

--- src/JBSips.sol
+++ src/JBSips.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/* 
+import {IJBSplitAllocator} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBSplitAllocator.sol";
+import {IJBPaymentTerminal} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPaymentTerminal.sol";
+import {IJBDirectory} from '@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBDirectory.sol';
+import {IJBOperatorStore} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBOperatorStore.sol";
+import {IJBController3_1} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBController3_1.sol";
+
+import {JBOperatable} from "@jbx-protocol/juice-contracts-v3/contracts/abstract/JBOperatable.sol";
+
+import { ISablierV2ProxyTarget } from "@sablier/v2-periphery/interfaces/ISablierV2ProxyTarget.sol";
+import { IPRBProxy, IPRBProxyRegistry } from "@sablier/v2-periphery/types/Proxy.sol";
+import { ISablierV2ProxyPlugin } from "@sablier/v2-periphery/interfaces/ISablierV2ProxyPlugin.sol";
+
+import { ISablierV2LockupDynamic } from "@sablier/v2-core/interfaces/ISablierV2LockupDynamic.sol";
+import { ISablierV2LockupLinear } from "@sablier/v2-core/interfaces/ISablierV2LockupLinear.sol";
+
+import {ERC165, IERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import { IERC20 } from "@sablier/v2-core/types/Tokens.sol";
+
+import { IAllowanceTransfer, Permit2Params } from "@sablier/v2-periphery/types/Permit2.sol";
+ */
+
+import {JBSablier} from "../src/abstract/JBSablier.sol";
+
 import {IJBSplitAllocator} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBSplitAllocator.sol";
 import {IJBPaymentTerminal} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPaymentTerminal.sol";
 import {IJBDirectory} from '@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBDirectory.sol';
@@ -38,7 +62,7 @@ import { IAllowanceTransfer, Permit2Params } from "@sablier/v2-periphery/types/P
  *         interact with Sablier v2 streams of which they are the beneficiary.
  * @dev -- notes n stuff --
 */
-contract JBSips is IJBSplitAllocator, JBOperatable, ERC165 {
+contract JBSips is JBSablier, JBOperatable, IJBSplitAllocator, ERC165 {
 
     //*********************************************************************//
     // --------------------------- custom errors ------------------------- //
@@ -51,27 +75,15 @@ contract JBSips is IJBSplitAllocator, JBOperatable, ERC165 {
     //*********************************************************************//
 
     //*********************************************************************//
-    // --------------------- public constant properties ------------------ //
-    //*********************************************************************//
-
-    IPRBProxy public proxy;
-    IPRBProxyRegistry public constant PROXY_REGISTRY = IPRBProxyRegistry(0x584009E9eDe26e212182c9745F5c000191296a78);
-    ISablierV2ProxyPlugin public immutable proxyPlugin;
-    IERC20 public constant DAI = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    IAllowanceTransfer public constant PERMIT2 = IAllowanceTransfer(0x000000000022D473030F116dDEE9F6B43aC78BA3);
-    
-    ISablierV2LockupLinear public sablier;
-    ISablierV2ProxyTarget public immutable proxyTarget;
-
-    IJBDirectory public directory;
-    uint256 public immutable projectId;
-    IJBController3_1 public controller;
-
-    //*********************************************************************//
     // --------------------- public stored properties -------------------- //
     //*********************************************************************//
 
     mapping(address => uint256) public idByAddress;
+    address[] public benefics;
+
+    //*********************************************************************//
+    // -------------------------- constructor ---------------------------- //
+    //*********************************************************************//
 
     constructor(
         uint256 _projectId, 
@@ -81,23 +93,20 @@ contract JBSips is IJBSplitAllocator, JBOperatable, ERC165 {
         ISablierV2ProxyPlugin _proxyPlugin,
         ISablierV2ProxyTarget _proxyTarget,
         IJBController3_1 _controller
-    )   JBOperatable(_operatorStore) 
+        )
+        JBSablier(
+        _projectId,
+        _directory,
+        _sablier,
+        _proxyPlugin,
+        _proxyTarget,
+        _controller)
+         JBOperatable(_operatorStore)
     {
-        projectId = _projectId;
-
-        /// @dev Mainnet JBDirectory as of 8/9/23: 0x65572FB928b46f9aDB7cfe5A4c41226F636161ea
-        directory = _directory;
-
-        /// @dev Mainnet SablierV2LockupLinear as of 8/9/23: 0xB10daee1FCF62243aE27776D7a92D39dC8740f95
-        sablier = _sablier;
-
-        /// @dev Proxy deployer with plugin https://docs.sablier.com/contracts/v2/guides/proxy-architecture/deploy
-        proxyPlugin = _proxyPlugin;
-
-        proxyTarget = _proxyTarget;
-
-        controller = _controller;
+       /* --- */
     }
+
+
 
     /// @notice Indicates if this contract adheres to the specified interface.
     /// @dev See {IERC165-supportsInterface}.
@@ -106,6 +115,7 @@ contract JBSips is IJBSplitAllocator, JBOperatable, ERC165 {
     function supportsInterface(bytes4 _interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
         return _interfaceId == type(IJBSplitAllocator).interfaceId || super.supportsInterface(_interfaceId);
     }
+
 
     /// @notice Called by a project's payout (JBTerminal) or reserved token distribution split (JBController)
     /// @dev See https://docs.juicebox.money/dev/learn/glossary/split-allocator/
@@ -120,6 +130,13 @@ contract JBSips is IJBSplitAllocator, JBOperatable, ERC165 {
 
         /* // Logic for reserved token ditribution split (bonus implementation, not the focus rn)
         if (directory.controllerOf(_data.projectId) == msg.sender) {} */
+
+    }
+
+    function batchDeploy() external returns (uint256[] memory streamIds) {
+        if (address(proxy) == address(0)) deployProxyAndInstallPlugin();
+
+        uint256 batchSize = benefics.length;
 
     }
 
